0c50f73ceffeddd243d072e17a50b04ea7a36c3c
JoaquinFurta <joaquinfurta2@gmail.com>
2023-01-20 15:24:10 -0800

    fixing_output

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,8 +5,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif len(argv) == 2:
         print("1 argument:")
-        print("{}".format(argv[1]))
     else:
         print("{} arguments:".format(len(argv) - 1))
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
