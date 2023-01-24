4c453f3f05d023d9234381d4521578f2befd78a3
JoaquinFurta <joaquinfurta2@gmail.com>
2023-01-24 15:30:45 -0800

    fixing_string_format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        print(' '.join(map(str, i)),end="$\n")
-        
+       for i in matrix:
+            list(map(lambda i:print("{:d}".format(i),end=" "), i))
+            print() 
