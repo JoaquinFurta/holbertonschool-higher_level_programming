1ad2403fcaf1be0288310e674167f09772de4bff
JoaquinFurta <joaquinfurta2@gmail.com>
2023-01-24 14:31:50 -0800

    adding_tasks_0_to_12

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/jpython3
+
+def print_list_integer(my_list=[]):
+
+    for elements in my_list:
+        print("{}".format(elements))
---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,6 @@
+#!/bin/usr/python3
+def element_at(my_list, idx):
+    if 0 > idx or idx > (len(my_list) - 1):
+        return None
+    else:
+        return my_list[idx]
