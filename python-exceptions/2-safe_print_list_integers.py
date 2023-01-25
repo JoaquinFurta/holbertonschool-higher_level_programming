a744fac9f1dd41df386020d40abbf4729721354c
JoaquinFurta <joaquinfurta2@gmail.com>
2023-01-25 15:15:57 -0800

    fixing_wrong_name_of_variable

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     iterr = 0
-    nb_print = i
+    nb_print = iterr
     while True:
         try:
             if iterr == x:
                 print()
                 return nb_print
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[iterr]), end="")
             iterr += 1
             nb_print += 1
         except (TypeError, ValueError):
