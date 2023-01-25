f4bec7a97006a28128d5076a1cff4f03ce23a707
JoaquinFurta <joaquinfurta2@gmail.com>
2023-01-25 15:14:09 -0800

    task_2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    iterr = 0
+    nb_print = i
+    while True:
+        try:
+            if iterr == x:
+                print()
+                return nb_print
+            print("{:d}".format(my_list[i]), end="")
+            iterr += 1
+            nb_print += 1
+        except (TypeError, ValueError):
+            iterr += 1
