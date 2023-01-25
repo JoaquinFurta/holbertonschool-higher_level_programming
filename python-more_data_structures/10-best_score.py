6f887851ccb6b0032237c8803497b8a149752bfe
JoaquinFurta <joaquinfurta2@gmail.com>
2023-01-25 13:36:37 -0800

    fixing

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     maxx = 0
+    person = None
     if a_dictionary is None:
         return None
     for key, value in a_dictionary.items():
--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
 
     rom_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
-    to_num = [roman_num[elem] for elem in roman_string if elem in roman_num]
+    to_num = [rom_num[elem] for elem in roman_string if elem in rom_num]
 
     resultado = 0
 
