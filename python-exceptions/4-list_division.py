9b604343efa884e3cc617025b147ce941df715b6
JoaquinFurta <joaquinfurta2@gmail.com>
2023-01-26 14:55:14 -0800

    task_4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    listdiv = []
+    for i in range(list_length):
+        try:
+            div = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            div = 0
+            print("wrong type")
+        except ZeroDivisionError:
+            div = 0
+            print("division by 0")
+        except (IndexError, RuntimeError):
+            print("out of range")
+            div = 0
+            break
+        finally:
+            listdiv.append(div)
+    return(listdiv)
