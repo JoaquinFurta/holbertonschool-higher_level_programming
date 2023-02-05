3e9fad149f56cca6306d54de1411374fb056a71f
JoaquinFurta <joaquinfurta2@gmail.com>
2023-02-05 12:38:05 -0800

    tasks from 0 to 6

---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""it  Creates and empty class"""
+
+
+class Square:
+    """Square class"""
+    pass
---
+++ python-classes/6-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" it creates a Squeare class """
+
+
+class Square:
+    """
+    Square class
+    args: size of the square
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return(self.__position)
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2 or\
+           not all(isinstance(idx, int) for idx in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size <= 0:
+            print()
+        else:
+            for idx in range(self.__position[1]):
+                print()
+            string = "".join([" " for idx in range(self.__position[0])])
+            string += "".join(["#" for idx in range(self.__size)])
+            for idx in range(self.__size):
+                print(string)
