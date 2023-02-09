3990bcb768cc798ac6c38960d0f9d6e7c162f23b
JoaquinFurta <joaquinfurta2@gmail.com>
2023-02-09 08:29:00 -0800

    adding.documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,13 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+
+"""Adding function"""
+
 
 def add_integer(a, b=98):
+    """Args to add:
+            (a),(b)
+       if they are not integers or floats an Error wuold be raised
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     elif not isinstance(b, int) and not isinstance(b, float):
