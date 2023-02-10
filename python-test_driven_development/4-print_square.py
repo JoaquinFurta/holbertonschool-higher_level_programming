9c732a00cdf725834c91f0a8526d266afb665b3c
JoaquinFurta <joaquinfurta2@gmail.com>
2023-02-10 10:48:51 -0800

    task_4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" function that prints a square with the character #"""
+
+
+def print_square(size):
+
+    """ prints squeare of size: size"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    strn = "".join("#" for idx in range(size))
+    print("".join(strn + "\n" if idx != size - 1
+                  else strn for idx in range(size)))
