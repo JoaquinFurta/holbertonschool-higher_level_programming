99118617d507c4c8f43a7025bc3948d267af156c
JoaquinFurta <joaquinfurta2@gmail.com>
2023-02-10 08:44:03 -0800

    fixing_bug

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -19,7 +19,7 @@ def matrix_divided(matrix, div):
 
     if not all(all(isinstance(elem, (int, float)) for elem in row)
                for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) "
-                        + "of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists)" + 
+                        " of integers/floats")
 
     return [[round(elem / div, 2) for elem in row] for row in matrix]
