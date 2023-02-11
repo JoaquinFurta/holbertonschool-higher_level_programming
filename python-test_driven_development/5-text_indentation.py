ad06260dd945da1ea9b6b44e467d32dff7651205
JoaquinFurta <joaquinfurta2@gmail.com>
2023-02-11 13:44:09 -0800

    task_5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" add newlines to a text when it contains (., ?, :)"""
+
+
+def text_indentation(text):
+    """ add newlines to a text when it contains (., ?, :)"""
+
+    if not isinstance(text, str):
+        TypeError("text must be a string")
+    print("".join([elem if elem not in ".?:" else elem + "\n\n"
+                   for elem in text]).replace("\n ", "\n"), end="")
