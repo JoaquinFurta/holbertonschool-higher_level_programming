11161c31a124bc5e719cd5bc9c03b7f2cbb5620c
JoaquinFurta <joaquinfurta2@gmail.com>
2023-02-12 10:18:29 -0800

    task_10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Class Stuedent with to_json method"""
+
+
+class Student:
+    """Class Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if not attrs:
+            return self.__dict__
+        else:
+            return {val: self.__dict__[val] for val in attrs
+                    if val in self.__dict__}
