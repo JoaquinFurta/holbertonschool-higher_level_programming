84a288b6916f10b597eb6e9b56f2d7955eb924b9
JoaquinFurta <joaquinfurta2@gmail.com>
2023-04-15 15:54:38 -0700

    fixing output strings

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@
 const process = require('process');
 
 if (isNaN(process.argv[2])) {
-  console.log('Not a Number');
+  console.log('Not a number');
 } else {
   console.log('My number: ' + process.argv[2]);
 }
