bc935026780038d5d5d907991db4c72b9521552a
JoaquinFurta <joaquinfurta2@gmail.com>
2023-04-16 12:50:38 -0700

    fixing

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+  const array = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    array.push(list[i]);
+  }
+  return array;
 };
