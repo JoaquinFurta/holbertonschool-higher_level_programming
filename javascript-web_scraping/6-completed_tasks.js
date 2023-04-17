67e812e5a7de5fe96476442bde446912b0f68429
JoaquinFurta <joaquinfurta2@gmail.com>
2023-04-17 16:09:06 -0700

    fix_border _cases

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -8,12 +8,17 @@ request.get(process.argv[2], (err, response, body) => {
   let count = 0;
   const res = {};
   const obj = JSON.parse(body);
-  for (let i = 1; i <= 10; i++) {
-    obj.forEach((elem) => {
-      if (elem.completed === true && i === elem.userId) count++;
-    });
-    res[i] = count;
-    count = 0;
+  try {
+    let i = 0;
+    while (1) {
+      obj.forEach((elem) => {
+        if (elem.completed === true && obj[i].userId === elem.userId) count++;
+      });
+      if (count !== 0) res[obj[i].userId] = count;
+      count = 0;
+      i++;
+    }
+  } catch (e) {
+    console.log(res);
   }
-  console.log(res);
 });
