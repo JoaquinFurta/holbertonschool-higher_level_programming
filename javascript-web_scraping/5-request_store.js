649f943bb2c1160d151641615145b012b0ba6a75
JoaquinFurta <joaquinfurta2@gmail.com>
2023-04-17 14:36:58 -0700

    fix_forget_to_delete_a_console.log

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 
 request.get(process.argv[2], (err, response, body) => {
   if (err) throw err;
-  console.log(body);
   fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
     if (err) throw err;
   });
