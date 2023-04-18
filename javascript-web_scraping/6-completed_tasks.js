2d48a1c17ae680f4bfd0cb7e44e8cee815ef3e6f
JoaquinFurta <joaquinfurta2@gmail.com>
2023-04-17 17:08:01 -0700

    fix

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -7,21 +7,15 @@ request.get(process.argv[2], (err, response, body) => {
   if (err) throw err;
   let count = 0;
   const res = {};
-  try {
-    const obj = JSON.parse(body);
-    let i = 0;
-    while (1) {
-      obj.forEach((elem) => {
-        if (elem.completed === true && obj[i].userId === elem.userId) count++;
-      });
-      if (count !== 0) res[obj[i].userId] = count;
-      count = 0;
-      i++;
-    }
-  } catch (e) {
-    console.log(res);
-    count = -1;
+  const obj = JSON.parse(body);
+  let i = 0;
+  while (i < obj.length) {
+    obj.forEach((elem) => {
+      if (elem.completed === true && obj[i].userId === elem.userId) count++;
+    });
+    if (count !== 0) res[obj[i].userId] = count;
+    count = 0;
+    i++;
   }
-  if (count !== -1)
-    console.log({});
+  console.log(res);
 });
